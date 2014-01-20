92d41316d743b66092b8443e741bc253bf12221c
Tadayuki kusakabe <kusakabe@gworks.jp>
2014-01-20 18:16:59 +0900

    2014012001
    add sysmente

--- dbsettings.php
+++ dbsettings.php
@@ -17,6 +17,21 @@ UNIQUE (
 )
 ) ENGINE = MYISAM ;
 
+
+CREATE TABLE IF NOT EXISTS `sys_mente` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `directry` varchar(250) NOT NULL DEFAULT '',
+  `mente` enum('yes','no') NOT NULL,
+  `hyoji` enum('yes','no') NOT NULL,
+  `hyoji_jun` int(11) NOT NULL DEFAULT '0',
+ `created` DATETIME  NULL ,
+ `modified` DATETIME  NULL ,
+ KEY `no` (`no`)
+) ENGINE=MyISAM  COMMENT='システムファイル毎のメンテナンス情報' ;
+
+
+
+
 */
 
 
